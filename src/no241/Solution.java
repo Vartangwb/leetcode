ac58493bc5668a3b7ababec17679e7116fce90de
twb <948328306@qq.com>
2017-05-15 16:29:25 +0800

    241 分治，添加括号问题

---
+++ src/no241/Solution.java
@@ -0,0 +1,48 @@
+package no241;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by twb on 2017/5/14.
+ */
+public class Solution {
+
+
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> res = new ArrayList<>();
+        int len = input.length();
+        for(int i = 0; i < len; i++){
+            char c = input.charAt(i);
+            if(c == '+' || c == '-' || c == '*'){
+                List<Integer> left = diffWaysToCompute(input.substring(0 , i));
+                List<Integer> right = diffWaysToCompute(input.substring(i+1 , input.length()));
+
+                for(int lef : left){
+                    for(int rig :right){
+                        System.out.println("left"+left.size());
+                        System.out.println("right"+right.size());
+                        if(c == '+'){
+                            res.add(lef+rig);
+                        }else if(c == '*'){
+                            res.add(lef*rig);
+
+                        }else if(c == '-'){
+                            res.add(lef-rig);
+                        }
+                    }
+                }
+            }
+        }
+        if(res.size() == 0)
+            res.add(Integer.valueOf(input));
+        return res;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.diffWaysToCompute("1+2+3*4");
+
+    }
+
+}
---
+++ src/no328/Solution.java
@@ -0,0 +1,46 @@
+package no328;
+
+
+/**
+ * Created by twb on 2017/5/14.
+ */
+public class Solution {
+
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+      }
+  }
+
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null)
+            return head;
+        ListNode curr = head;
+        ListNode secCurr = head.next;
+        ListNode secNode = secCurr;
+        while(secCurr!=null && secCurr.next!=null){
+            curr.next = curr.next.next;
+            secCurr.next = secCurr.next.next;
+            curr = curr.next;
+            secCurr = secCurr.next;
+        }
+        curr.next = secNode;
+        return head;
+    }
+    public void printLn(ListNode ls){
+        while(ls!=null){
+            System.out.println(ls.val);
+            ls = ls.next;
+        }
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode ls = new ListNode(1);
+        ls.next = new ListNode(2);
+        ls.next.next = new ListNode(3);
+        solution.printLn(solution.oddEvenList(ls));
+    }
+
+}
