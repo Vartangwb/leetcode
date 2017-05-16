791728fdf201ec61d3bcda4a1d0600ca035cdf30
twb <948328306@qq.com>
2017-05-16 14:41:45 +0800

    490.迷宫问题

---
+++ src/no046/Solution.java
@@ -0,0 +1,27 @@
+package no046;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by twb on 2017/5/14.
+ */
+public class Solution {
+    //public int findDuplicate(int[] nums) {
+
+   // }
+   public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+   }
+
+
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+    }
+
+}
