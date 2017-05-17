529920026b9d73c5ac9c86b7cc3a76028df7dda4
twb <948328306@qq.com>
2017-05-17 23:43:34 +0800

    386.数字的字典序排序 394.栈数字计算

---
+++ src/no386/Solution.java
@@ -0,0 +1,42 @@
+package no386;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by twb on 2017/5/14.
+ */
+public class Solution {
+
+
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> ls = new ArrayList<Integer>();
+        for(int i=1;i<=9;i++){
+            dfs(n, i, ls);
+        }
+        return ls;
+    }
+    public void dfs(int n, int curr, List<Integer> ls){
+        //if(curr > n)
+        //    return;
+        ls.add(curr);
+        for(int i = 0; i <=9;i++){
+            if(curr*10+i>n)
+                return;
+            dfs(n, curr*10+i, ls);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        List<Integer> ls  = solution.lexicalOrder(13);
+        for(Integer ll:ls){
+            System.out.println(ll);
+        }
+    }
+
+}
---
+++ src/no394/Solution.java
@@ -0,0 +1,57 @@
+package no394;
+
+
+import java.util.Stack;
+
+/**
+ * Created by twb on 2017/5/14.
+ */
+public class Solution {
+
+
+    public String decodeString(String s) {
+        Stack<String> stc = new Stack<>();
+        Stack<Integer> stc_num = new Stack<>();
+        String str = "";
+        int len = s.length();
+        int i = 0;
+        while(i<len){
+            char c = s.charAt(i);
+            if(c>='1' && c<='9'){
+                int count = c - '0';
+                i++;
+                while(s.charAt(i)>=0 && s.charAt(i)<=9){
+                    count = count*10+s.charAt(i)-'0';
+                    i++;
+
+                }
+                stc_num.push(count);
+            }else if(s.charAt(i) == '['){
+                stc.push(str);
+                i++;
+                str = "";
+            }else if(s.charAt(i) == ']'){
+                StringBuilder sb = new StringBuilder(stc.pop());
+                int couuu = stc_num.pop();
+                for(int ii = 0;ii < couuu; ii++) {
+                    sb.append(str);
+                }
+                str = sb.toString();
+                i++;
+            }else {
+                str+=s.charAt(i);
+                i++;
+            }
+        }
+        return str;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        System.out.println(solution.decodeString("2[abc]"));
+    }
+
+}
