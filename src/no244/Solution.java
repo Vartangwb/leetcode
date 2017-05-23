f022296e1894d5445526f7b54b1e5fb0104efd16
twb <948328306@qq.com>
2017-05-23 21:42:13 +0800

    244.单词间的距离

---
+++ src/no244/Solution.java
@@ -0,0 +1,28 @@
+package no244;
+
+/**
+ * Created by twb on 2017/5/23.
+ */
+public class Solution {
+
+    int shortestDistance(String[] words, String word1, String word2){
+        int curr1 = -1;
+        int curr2 = -1;
+        int min = Integer.MIN_VALUE;
+        for(int i = 0;i<words.length;i++){
+            if(word1.equals(words[i])){
+                curr1 = i;
+                if(curr2!=-1){
+                    min = Math.min(min, Math.abs(curr1 - curr2));
+                }
+            }else if(word2.equals(words[i])){
+                curr2 = i;
+                if(curr1!=-1){
+                    min = Math.min(min, Math.abs(curr1-curr2));
+                }
+            }
+        }
+        return min;
+    }
+
+}
