f11f8f607760dcc162f2983a7a831b0f1e39584a
Vartangwb <15168239640@163.com>
2017-05-26 14:33:28 +0800

    113.Path Sum 542.BFS最短路径

---
+++ src/no113/Solution.java
@@ -0,0 +1,58 @@
+package no113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by twb on 2017/5/26.
+ */
+public class Solution {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+
+        helper(root, 0,new ArrayList<>(), sum);
+        return res;
+
+    }
+
+    public void helper(TreeNode root, int currSum, List<Integer> ls, int sum){
+
+            ls.add(root.val);
+            if(currSum+root.val>sum)
+                return;
+            if(root.left==null && root.right==null){
+                if(currSum+ root.val == sum){
+                    res.add(new ArrayList<>(ls));
+                }
+            }
+            if(root.left!=null){
+                helper(root.left, currSum+root.val, ls, sum);
+            }
+            if(root.right!=null){
+                helper(root.right, currSum+root.val, ls, sum);
+            }
+            ls.remove(ls.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(1);
+        for(List<Integer> ls:solution.pathSum(root, 4)){
+            for(Integer ll: ls){
+                System.out.print(ll+" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ src/no542/Solution.java
@@ -0,0 +1,54 @@
+package no542;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by twb on 2017/5/26.
+ */
+public class Solution {
+
+    //广度优先搜索可以用来解决最短路径问题；
+    int dir[][] = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+    public int[][] updateMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(matrix[i][j] == 0){
+                    queue.add(new int[]{i,j});
+                }else{
+                    matrix[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        while(queue.isEmpty() == false){
+            int tmp[] = queue.poll();
+            for(int i = 0;i<4;i++){
+                int currx = tmp[0]+dir[i][0];
+                int curry = tmp[1]+dir[i][1];
+                if(currx>=0 && currx<m && curry>=0 && curry<n){
+                    if(matrix[currx][curry]>matrix[tmp[0]][tmp[1]]+1){
+                        matrix[currx][curry] = matrix[tmp[0]][tmp[1]]+1;
+                        queue.add(new int[]{currx, curry});
+                    }
+                }
+            }
+        }
+        return matrix;
+    }
+
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int ttt[][] = solution.updateMatrix(new int[][]{{0,0,0},{0,1,0},{1,1,1}});
+        for(int ii[]:ttt){
+            for(int i:ii){
+                System.out.print(i+" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
