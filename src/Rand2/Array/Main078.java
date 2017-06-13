a636f62adfcdd632caa09d19e4a6b24e2f00b13e
Vartangwb <15168239640@163.com>
2017-06-13 13:42:12 +0800

    Array

---
+++ src/Rand2/Array/Main078.java
@@ -0,0 +1,16 @@
+package Array;
+
+import java.util.List;
+
+/**
+ * Created by twb on 2017/6/7.
+ */
+public class Main078 {
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        return null;
+    }
+    public static void main(String[] args) {
+        subsets(new int[]{2,1,0});
+        }
+}
